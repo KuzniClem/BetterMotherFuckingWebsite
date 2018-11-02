0a34b22b7f0590b12c60a73e29a3b18e9620333c
Pyki <clement@kuzniewycz.fr>
2018-11-02 12:24:14 +0100

    ajout de la popup

---
+++ popup.js
@@ -0,0 +1,5 @@
+setTimeout(function() {
+	if (confirm('Te laisserais-tu tenter par MOTHERFUCKING xxxvidsxxx ?')) {
+		window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+	}
+}, 10000);
